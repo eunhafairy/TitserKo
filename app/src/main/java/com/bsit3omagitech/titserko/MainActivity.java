0a271e891b36deac1818f9980861599ba062ce7f
iver <ipesiganifc@gmail.com>
2021-12-22 16:40:26 +0800

    converted Main Activity as constraint layout

--- app/src/main/java/com/bsit3omagitech/titserko/MainActivity.java
+++ app/src/main/java/com/bsit3omagitech/titserko/MainActivity.java
@@ -1,8 +1,10 @@
 package com.bsit3omagitech.titserko;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -23,11 +25,11 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Spinner spnr_profile;
-    ImageView iv_study;
-    LinearLayout ll_landing, ll_profile;
+    ImageView iv_study,imageView;
     Button tv_createProfile;
     Button btn_confirm;
     String usernameSelected;
+    TextView textView;
     DataBaseHelper db;
     MediaPlayer mp;
     @Override
@@ -44,18 +46,36 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         db = new DataBaseHelper(this);
         usernameSelected = "";
-        spnr_profile = (Spinner) findViewById(R.id.spnr_profile);
-        spnr_profile.setOnItemSelectedListener(this);
+
+
 
         //load spinner data
-        loadSpinnerData();
+
+
+        //show the group of views for welcome
+        imageView = findViewById(R.id.imageView);
         iv_study = (ImageView) findViewById(R.id.iv_study);
-        ll_landing = (LinearLayout) findViewById(R.id.ll_landing);
-        ll_landing.setVisibility(View.VISIBLE);
-        ll_profile = (LinearLayout) findViewById(R.id.ll_profile);
-        ll_profile.setVisibility(View.GONE);
+
+        imageView.setVisibility(View.VISIBLE);
+        iv_study.setVisibility(View.VISIBLE);
+
+        //show the group of views for signing in
+        spnr_profile = (Spinner) findViewById(R.id.spnr_profile);
+        textView = findViewById(R.id.textView);
         tv_createProfile = findViewById(R.id.tv_createProfile);
         btn_confirm = (Button) findViewById(R.id.btn_confirm);
+        spnr_profile.setOnItemSelectedListener(this);
+        loadSpinnerData();
+
+
+        spnr_profile.setVisibility(View.GONE);
+        textView.setVisibility(View.GONE);
+        btn_confirm.setVisibility(View.GONE);
+        tv_createProfile.setVisibility(View.GONE);
+
+
+
+
         mp = new MediaPlayer();
 
         Uri mediaPath = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.bensound_bgm);
@@ -102,8 +122,15 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             public void onClick(View v) {
 
                 if(db.userExists()){
-                    ll_landing.setVisibility(View.GONE);
-                    ll_profile.setVisibility(View.VISIBLE);
+                    spnr_profile.setVisibility(View.VISIBLE);
+                    textView.setVisibility(View.VISIBLE);
+                    btn_confirm.setVisibility(View.VISIBLE);
+                    tv_createProfile.setVisibility(View.VISIBLE);
+
+                    imageView.setVisibility(View.GONE);
+                    iv_study.setVisibility(View.GONE);
+
+
 
                 }
                 else{
@@ -175,22 +202,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String text = parent.getItemAtPosition(position).toString();
         usernameSelected = text;
-    } public void onBackPressed() {
-
-        if(ll_profile.getVisibility() == View.VISIBLE){
-
-            ll_profile.setVisibility(View.GONE);
-            ll_landing.setVisibility(View.VISIBLE);
-        }
-        else{
-            System.exit(0);
-            finish();
-
-        }
-
-
-
-
     }
 
     @Override
@@ -198,6 +209,46 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     }
 
+    public void onBackPressed() {
+
+        if( spnr_profile.getVisibility() == View.VISIBLE){
+
+            spnr_profile.setVisibility(View.GONE);
+            textView.setVisibility(View.GONE);
+            btn_confirm.setVisibility(View.GONE);
+            tv_createProfile.setVisibility(View.GONE);
+
+            imageView.setVisibility(View.VISIBLE);
+            iv_study.setVisibility(View.VISIBLE);
+
+        }
+        else{
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Exit App?")
+                    .setMessage("Are you sure you want to quit?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                            System.exit(0);
+                        }
+                    }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // do nothing
+                }
+            })
+                    .setCancelable(true);
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+
+        }
+
+
+
+
+    }
 
 
 
